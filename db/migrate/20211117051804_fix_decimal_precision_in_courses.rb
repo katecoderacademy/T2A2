61ffe082e9b5d1f3b6dd7e8755efdf6a18c7eda3
katecoderacademy <cam022111@coderacademy.edu.au>
2021-11-17 17:39:22 +1100

    pushing pre migration of DB regarding adding precision information to courses

---
+++ db/migrate/20211117051804_fix_decimal_precision_in_courses.rb
@@ -0,0 +1,12 @@
+class FixDecimalPrecisionInCourses < ActiveRecord::Migration[6.1]
+  def change
+    change_column :courses, :practice_management, :precision => 1,
+    change_column :courses, :substantive_law, :precision => 1, :scale => 20
+    change_column :courses, :ethics, :precision => 1, :scale => 20
+    change_column :courses, :professional_skills, :precision => 1
+    change_column :courses, :fee, :decimal, :precision => 2
+    add_column :courses, :url, :text
+    rename_column :courses, :time, :start_time
+    add_column :courses, :end_time, :time
+  end
+end
