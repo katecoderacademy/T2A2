296f5ec86d0745735a9c2aec21e5cac9b57801e9
katecoderacademy <cam022111@coderacademy.edu.au>
2021-11-23 18:12:23 +1100

    removed migration that broke Heroku

--- db/migrate/20211121115202_create_enrollment_id.rb
+++
@@ -1,9 +0,0 @@
-class CreateEnrollmentId < ActiveRecord::Migration[6.1]
-  def change
-      # add_column :enrollments, :enrollment_id, :integer
-      # rename_column :enrollments, :users_id, :user_id
-      # rename_column :enrollments, :courses_id, :course_id
-      t.timestamps
-    
-  end
-end
