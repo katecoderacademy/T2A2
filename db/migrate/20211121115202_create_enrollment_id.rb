c11f3230d5f1aac638f55d570a93304a7ed1df9f
katecoderacademy <cam022111@coderacademy.edu.au>
2021-11-23 19:58:07 +1100

    removing typo in migration

--- db/migrate/20211121115202_create_enrollment_id.rb
+++ db/migrate/20211121115202_create_enrollment_id.rb
@@ -3,7 +3,7 @@ class CreateEnrollmentId < ActiveRecord::Migration[6.1]
        add_column :enrollments, :enrollment_id, :integer
        rename_column :enrollments, :users_id, :user_id
        rename_column :enrollments, :courses_id, :course_id
-      t.timestamps
+
     
   end
 end
