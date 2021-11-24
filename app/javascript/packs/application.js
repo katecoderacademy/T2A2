0840ed5be083b8263326f827808a1af617f62837
katecoderacademy <cam022111@coderacademy.edu.au>
2021-11-24 21:58:36 +1100

    further modified bootstrap installation to fix heroku issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -9,6 +12,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
+import 'bootstrap';
+import '../stylesheets/application';
 
 Rails.start()
 Turbolinks.start()
